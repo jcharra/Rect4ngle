4bf9b100a5ab94ece7c72a8709e54646c1eb398a
Johannes Charra <johannes.charra@gmail.com>
2021-06-12 00:02:47 +0200

    add some bonus and malus stuff, improve looks

--- src/components/MainArea.tsx
+++ src/components/MainArea.tsx
@@ -1,4 +1,11 @@
-import { IonButton, IonCol, IonGrid, IonIcon, IonRow } from "@ionic/react";
+import {
+  CreateAnimation,
+  IonButton,
+  IonCol,
+  IonGrid,
+  IonIcon,
+  IonRow,
+} from "@ionic/react";
 import { arrowUndo, diamondOutline, trashOutline } from "ionicons/icons";
 import React, { useState } from "react";
 import "./MainArea.css";
@@ -16,10 +23,25 @@ const PRIMES = Array.from(Array(100))
   .map((_, idx) => idx)
   .filter((i) => i > 10 && isPrime(i));
 
-function Tappable({ value, onClick }: { value: number; onClick: Function }) {
+const PRIME_BONUS = 20;
+const PRIME_MALUS = -10;
+
+function Tappable({
+  value,
+  onClick,
+  disabled,
+}: {
+  value: number;
+  onClick: Function;
+  disabled: boolean;
+}) {
   return (
     <div className="block" onClick={() => onClick()}>
-      <VisualTupel n={value} offset={0} />
+      <VisualTupel
+        colorClass={disabled ? "disabledDot" : "activeDot"}
+        n={value}
+        offset={0}
+      />
     </div>
   );
 }
@@ -28,14 +50,24 @@ function CurrentCalculation({ num }: { num: number }) {
   return <div className="currentCalculation">{num}</div>;
 }
 
-function Dot() {
-  return <div className="dot"></div>;
+function Dot({ colorClass }: { colorClass: string }) {
+  return <div className={`dot ${colorClass}`}></div>;
 }
 
-function VisualTupel({ n, offset }: { n: number; offset: number }) {
-  const dots = Array.from(Array(n)).map((_, idx) => <Dot key={idx} />);
+function VisualTupel({
+  n,
+  offset,
+  colorClass,
+}: {
+  n: number;
+  offset: number;
+  colorClass: string;
+}) {
+  const dots = Array.from(Array(n)).map((_, idx) => (
+    <Dot colorClass={colorClass} key={idx} />
+  ));
   return (
-    <div className="verticalTupel">
+    <div>
       {dots}
       <div className="tupelCaption">{offset + n}</div>
     </div>
@@ -46,7 +78,9 @@ function CalculationMainArea({ summands }: { summands: number[] }) {
   let tupels: JSX.Element[] = [];
   let sum = 0;
   summands.forEach((n, idx) => {
-    tupels.push(<VisualTupel key={idx} n={n} offset={sum} />);
+    tupels.push(
+      <VisualTupel key={idx} n={n} offset={sum} colorClass="activeDot" />
+    );
     sum += n;
   });
 
@@ -57,18 +91,75 @@ function CalculationMainArea({ summands }: { summands: number[] }) {
   );
 }
 
-function CalculationFeedback({ diamonds }: { diamonds: number }) {
+function Score({
+  diamonds,
+  delta,
+  comment,
+}: {
+  diamonds: number;
+  delta: number | undefined;
+  comment: string;
+}) {
   return (
-    <div className="feedback">
-      <IonIcon size="4x" icon={diamondOutline}></IonIcon> &nbsp;{diamonds}
-    </div>
+    <>
+      <div className="score">
+        <IonIcon size="4x" icon={diamondOutline}></IonIcon> &nbsp;{diamonds}
+      </div>
+      <div className="delta">
+        {delta && (
+          <CreateAnimation
+            duration={1000}
+            iterations={1}
+            fromTo={[
+              {
+                property: "transform",
+                fromValue: "translateY(0px)",
+                toValue: `translateY(${delta > 0 ? -100 : 100}px)`,
+              },
+              { property: "opacity", fromValue: "1", toValue: "0" },
+            ]}
+            play={true}
+          >
+            <div className={`${delta > 0 ? "bonus" : "malus"}`}>
+              {!!delta && delta > 0 ? "+" : ""}
+              {delta}
+            </div>
+          </CreateAnimation>
+        )}
+      </div>
+      <div className="comment">
+        {!!comment && (
+          <CreateAnimation
+            duration={1000}
+            iterations={1}
+            fromTo={[{ property: "opacity", fromValue: "1", toValue: "0" }]}
+            play={true}
+          >
+            <div className={`${comment ? "good" : "bad"}`}>{comment}</div>
+          </CreateAnimation>
+        )}
+      </div>
+    </>
   );
 }
 
-function TappableTuples({ add }: { add: Function }) {
-  const tappables = [2, 3, 4, 5, 6, 7, 8, 9, 10].map((n) => (
-    <Tappable value={n} onClick={() => add(n)} />
-  ));
+function TappableTuples({
+  add,
+  selectedValue,
+}: {
+  add: Function;
+  selectedValue: number | null;
+}) {
+  const tappables = [2, 3, 4, 5, 6, 7, 8, 9].map((n) => {
+    const disabled = !!selectedValue && n !== selectedValue;
+    return (
+      <Tappable
+        value={n}
+        onClick={() => !disabled && add(n)}
+        disabled={disabled}
+      />
+    );
+  });
 
   return <div className="headerTuples">{tappables}</div>;
 }
@@ -77,6 +168,8 @@ export function MainArea() {
   const [summands, setSummands] = useState<number[]>([]);
   const [num, setNum] = useState(0);
   const [diamonds, setDiamonds] = useState(0);
+  const [delta, setDelta] = useState<number | undefined>();
+  const [comment, setComment] = useState<string>("");
 
   const add = (n: number) => {
     setSummands((oldVal: number[]) => {
@@ -90,7 +183,7 @@ export function MainArea() {
 
   function generateRandomNumber() {
     if (Math.random() > 0.1) {
-      const [fac1, fac2] = [getRandomInt(10), getRandomInt(10)];
+      const [fac1, fac2] = [getRandomInt(9), getRandomInt(10)];
       return fac1 * fac2;
     } else {
       const idx = Math.floor(Math.random() * PRIMES.length);
@@ -120,31 +213,34 @@ export function MainArea() {
   const check = () => {
     const sum = summands.reduce((a, b) => a + b, 0);
     if (sum === num) {
-      bonus(1);
+      addDelta(summands[0], "Correct!");
     } else {
-      malus(5);
+      addDelta(-5, "Wrong!");
     }
     newNum();
   };
 
   const checkPrime = () => {
     if (PRIMES.indexOf(num) > -1) {
-      bonus(20);
+      addDelta(PRIME_BONUS, "PRIME!");
     } else {
-      malus(10);
+      addDelta(PRIME_MALUS, "Nope");
     }
     newNum();
   };
 
-  const bonus = (n: number) => {
-    setDiamonds((oldVal) => oldVal + n);
-  };
-  const malus = (n: number) => {
-    setDiamonds((oldVal) => Math.max(0, oldVal - n));
+  const addDelta = (delta: number, comment: string = "") => {
+    setDelta(delta);
+    setTimeout(() => setDelta(undefined), 1000);
+
+    setComment(comment);
+    setTimeout(() => setComment(""), 1000);
+
+    setDiamonds((oldVal) => Math.max(0, oldVal + delta));
   };
 
   const skip = () => {
-    malus(1);
+    addDelta(-1);
     newNum();
   };
 
@@ -156,15 +252,22 @@ export function MainArea() {
             <CurrentCalculation num={num} />
           </IonCol>
           <IonCol size="9">
-            <TappableTuples add={add} />
+            <TappableTuples
+              add={add}
+              selectedValue={summands.length > 0 ? summands[0] : null}
+            />
           </IonCol>
         </IonRow>
         <IonRow>
           <IonCol size="3">
-            <CalculationFeedback diamonds={diamonds} />
+            <Score diamonds={diamonds} delta={delta} comment={comment} />
           </IonCol>
           <IonCol size="9" className="ion-text-center">
-            <IonButton color="success" onClick={() => check()}>
+            <IonButton
+              disabled={summands.length < 2}
+              color="success"
+              onClick={() => check()}
+            >
               Check
             </IonButton>
             <IonButton onClick={() => skip()}>Skip</IonButton>
