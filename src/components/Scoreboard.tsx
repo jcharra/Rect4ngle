7952d465ec43db179bc2ac76850d685b944b0340
Johannes Charra <johannes.charra@gmail.com>
2021-06-20 21:06:51 +0200

    styling stuff

--- src/components/Scoreboard.tsx
+++ src/components/Scoreboard.tsx
@@ -13,41 +13,50 @@ export default function Scoreboard({
   return (
     <>
       <div className="score">
-        <IonIcon size="4x" icon={diamondOutline}></IonIcon> &nbsp;{diamonds}
-      </div>
-      <div className="delta">
-        {delta && (
-          <CreateAnimation
-            duration={1000}
-            iterations={1}
-            fromTo={[
-              {
-                property: "transform",
-                fromValue: "translateY(0px)",
-                toValue: `translateY(${delta > 0 ? -100 : 100}px)`,
-              },
-              { property: "opacity", fromValue: "1", toValue: "0" },
-            ]}
-            play={true}
-          >
-            <div className={`${delta > 0 ? "bonus" : "malus"}`}>
-              {!!delta && delta > 0 ? "+" : ""}
-              {delta}
-            </div>
-          </CreateAnimation>
-        )}
-      </div>
-      <div className="comment">
-        {!!comment && (
-          <CreateAnimation
-            duration={1000}
-            iterations={1}
-            fromTo={[{ property: "opacity", fromValue: "1", toValue: "0" }]}
-            play={true}
-          >
-            <div className={`${comment ? "good" : "bad"}`}>{comment}</div>
-          </CreateAnimation>
-        )}
+        <div className="comment">
+          {!!comment ? (
+            <CreateAnimation
+              duration={1000}
+              iterations={1}
+              fromTo={[{ property: "opacity", fromValue: "1", toValue: "0" }]}
+              play={true}
+            >
+              <div className={`${comment ? "good" : "bad"}`}>{comment}</div>
+            </CreateAnimation>
+          ) : (
+            <div className="invisible">Comment</div>
+          )}
+        </div>
+        <div className="scoreContainer">
+          <div className="scoreIcon">
+            <IonIcon icon={diamondOutline}></IonIcon>
+          </div>
+          <div className="scoreNum">{diamonds}</div>
+        </div>
+        <div className="delta">
+          {delta ? (
+            <CreateAnimation
+              duration={1000}
+              iterations={1}
+              fromTo={[
+                {
+                  property: "transform",
+                  fromValue: "translateY(0px)",
+                  toValue: `translateY(${delta > 0 ? -100 : 100}px)`,
+                },
+                { property: "opacity", fromValue: "1", toValue: "0" },
+              ]}
+              play={true}
+            >
+              <div className={`${delta > 0 ? "bonus" : "malus"}`}>
+                {!!delta && delta > 0 ? "+" : ""}
+                {delta}
+              </div>
+            </CreateAnimation>
+          ) : (
+            <div className="invisible">foo</div>
+          )}
+        </div>
       </div>
     </>
   );
--- src/components/parts/Dot.tsx
+++ src/components/parts/Dot.tsx
@@ -1,3 +1,5 @@
+import "./Dot.css";
+
 export function Dot({ colorClass }: { colorClass: string }) {
   return <div className={`dot ${colorClass}`}></div>;
 }
