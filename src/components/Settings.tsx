f5351cbe48f9f87e190a75a17671e941d1089da5
Johannes Charra <johannes.charra@gmail.com>
2021-06-27 17:55:46 +0200

    add player selection

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,4 +1,5 @@
 import {
+  IonInput,
   IonItem,
   IonLabel,
   IonList,
@@ -11,6 +12,12 @@ import { useState } from "react";
 export default function Settings() {
   const [checked, setChecked] = useState(true);
 
+  const players = ["Peter", "Hans", ""];
+
+  const savePlayerName = (idx: number, s: string) => {
+    console.log("Saving player", idx, "as", s);
+  };
+
   return (
     <IonList lines="full">
       <IonItem>
@@ -19,26 +26,35 @@ export default function Settings() {
         </IonLabel>
       </IonItem>
       <IonItem>
-        <IonLabel slot="start">Spieler</IonLabel>
-        <IonSelect slot="end" interface="popover" value="immi">
-          <IonSelectOption value="immi">Immi</IonSelectOption>
-          <IonSelectOption value="judith">Judith</IonSelectOption>
+        <IonLabel slot="start">Aktiver Spieler</IonLabel>
+        <IonSelect slot="end" interface="popover" value={players[0]}>
+          {players.map((p) => {
+            return !!p ? (
+              <IonSelectOption value={p}>{p}</IonSelectOption>
+            ) : null;
+          })}
         </IonSelect>
       </IonItem>
+      {[0, 1, 2, 3, 4].map((i) => {
+        return (
+          <IonItem>
+            <IonLabel>Spieler {i + 1}</IonLabel>
+            <IonInput
+              slot="end"
+              type="text"
+              value={players[i]}
+              placeholder="Name eingeben"
+              onBlur={(e) => savePlayerName(i, e.target.value as string)}
+            ></IonInput>
+          </IonItem>
+        );
+      })}
       <IonItem>
         <IonLabel slot="start">Sprache</IonLabel>
         <IonSelect slot="end" interface="popover" value="deutsch">
           <IonSelectOption value="deutsch">deutsch</IonSelectOption>
           <IonSelectOption value="englisch">englisch</IonSelectOption>
         </IonSelect>
-      </IonItem>
-      <IonItem>
-        <IonLabel slot="start">Ton</IonLabel>
-        <IonToggle
-          slot="end"
-          checked={checked}
-          onClick={() => setChecked(!checked)}
-        />
       </IonItem>
     </IonList>
   );
