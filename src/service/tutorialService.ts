ef1d4b25f8582ce43549988617c81549a429a2fe
jcharra <johannes.charra@gmail.com>
2024-03-26 12:09:49 +0100

    reinsert tutorial nums

--- src/service/tutorialService.ts
+++ src/service/tutorialService.ts
@@ -1,8 +1,7 @@
 import { Preferences } from "@capacitor/preferences";
 
 const TUTORIAL_SEEN = "tutorialSeen";
-//export const TUTORIAL_NUMBERS = [20, 32, 25, 9, 17, 36, 11, 30];
-export const TUTORIAL_NUMBERS = [17, 36, 11, 30];
+export const TUTORIAL_NUMBERS = [20, 32, 25, 9, 17, 36, 11, 30];
 
 export async function markAsSeen() {
   await Preferences.set({
